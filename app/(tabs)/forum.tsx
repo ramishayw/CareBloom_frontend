8d884d4f2aac6cdb3738b97f5af7f5b2a7df266f
Ramisha Ekanayaka <ramishayw@gmail.com>
2025-06-17 21:16:47 +0530

    broke the home page to components

--- app/(tabs)/forum.tsx
+++ app/(tabs)/forum.tsx
@@ -1,12 +1,16 @@
+import Header from "@/components/Header";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Forum() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Text>Forum Page</Text>
-    </View>
+    <>
+      <Header />
+      <View
+        style={styles.container}
+      >
+        <Text>Forum Page</Text>
+      </View>
+    </>
   );
 }
 
